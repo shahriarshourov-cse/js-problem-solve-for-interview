2f4ab0f28589acb792cfc4d2e8af19a10c8dde5c
Shahriar Shourov <shahriarshourov@github.com>
2023-08-08 13:18:47 +0600

    solved

---
+++ 02-palindromes.js
@@ -0,0 +1,11 @@
+let myString = "abba";
+function palindrome(myString) {
+  let reversal = myString.split("").reverse().join("");
+  return myString === reversal;
+}
+
+// Call the function and store the result
+let isPalindrome = palindrome(myString);
+
+// Display the result using console.log
+console.log(isPalindrome);
