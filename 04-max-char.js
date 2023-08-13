0eb24797411ff1a4bdfdd29b7b1cd65ee36fe3dd
Shahriar Shourov <shahriarshourov@github.com>
2023-08-13 16:32:57 +0600

    done

---
+++ 04-max-char.js
@@ -0,0 +1,7 @@
+//find Maximum character in given string
+function getMaxChar(str) {
+  let newStr = str.split("").forEach((element) => {
+    console.log(newStr);
+  });
+}
+getMaxChar("Universe");
